cb8f205354526862b0add968fd91c199fc62496c
NSLogName <losersuq@gmail.com>
2017-03-26 00:35:17 +0800

    2017-03-25

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -1,12 +1,16 @@
 package com.test;
 
+import com.machinepublishers.jbrowserdriver.JBrowserDriver;
+import com.machinepublishers.jbrowserdriver.Settings;
+import com.machinepublishers.jbrowserdriver.Timezone;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,36 +21,49 @@ import java.util.regex.Pattern;
  * Time: 10:12
  ***************************/
 public class Test {
-    static final String URL = "http://www.qqzhibo.tk";
-    static List<String> idList = new ArrayList<>();
+    static final String TARGET_URL = "http://www.qqzhibo.tk";
+    static String htmlSource;
+    static Map dataMap = new HashMap();
 
     private void spiderHTML() throws Exception {
+        /***
+         * 模拟浏览器获取动态网页内容
+         */
+        JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
+        driver.get(TARGET_URL);
+        htmlSource = driver.getPageSource();
+        driver.quit();
+
+        /***
+         * 利用JSoup抓取网页内有用信息
+         */
         try {
-            Document doc = Jsoup.connect(URL).get();
+            Document doc = Jsoup.parseBodyFragment(htmlSource,TARGET_URL);
+//            System.out.println(doc);
 
-            Element content = doc.getElementById("pageone");
-            Elements links = content.getElementsByTag("li");
+            Elements content = doc.select("div[role=main]");
+            Elements liClass = content.select("li[class]");
+//            System.out.println(liClass);
 
-            for (Element link : links) {
+            for (Element link : liClass) {
                 String str = link.className();
+//                System.out.println(str);
                 String regEx="[^0-9]";
                 Pattern p = Pattern.compile(regEx);
                 Matcher m = p.matcher(str);
-                idList.add(m.replaceAll("").trim());
-            }
-
-            for (String str : idList) {
-                if (str.length() > 8) {
-                    Element content1 = doc.getElementById(str);
-                    Elements link3 = content1.getElementsByClass("link");
-                    String linkstr = link3.attr("href");
-                    System.out.println(link3);
+                String matchId = (String) m.replaceAll("").trim();
+                if (matchId.length() == 10)
+                {
+                    System.out.println(matchId);
                 }
             }
+
         }
         catch (Exception e){
             e.printStackTrace();
         }
+
+
     }
 
     public static void main(String[] args) throws Exception {
