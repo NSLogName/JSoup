3138d388c1864b5f2cbd84f701c5ae41b7d7f7a0
nslogname <losersuq@gmail.com>
2017-03-26 04:04:12 +0800

    2017-03-26

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -9,7 +9,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,16 +24,23 @@ import java.util.regex.Pattern;
  ***************************/
 public class Test {
     static final String TARGET_URL = "http://www.qqzhibo.tk";
+    static final String MATCH_DATE = "date";
+    static final String MATCH_BATTLE = "battle";
+    static final String MATCH_STATUS = "status";
     static String htmlSource;
-    static Map dataMap = new HashMap();
+    static List<Map> dataList = new ArrayList<Map>();
+    static List<String> matchIdList = new ArrayList();
+    Elements matchMessageHeader;
+    Elements matchMessageContent;
 
     private void spiderHTML() throws Exception {
         /***
          * 模拟浏览器获取动态网页内容
          */
-        JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
+        JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.ASIA_SHANGHAI).build());
         driver.get(TARGET_URL);
         htmlSource = driver.getPageSource();
+//        System.out.println(htmlSource);
         driver.quit();
 
         /***
@@ -52,12 +61,39 @@ public class Test {
                 Pattern p = Pattern.compile(regEx);
                 Matcher m = p.matcher(str);
                 String matchId = (String) m.replaceAll("").trim();
-                if (matchId.length() == 10)
-                {
-                    System.out.println(matchId);
+                if (matchId.length() == 10) {
+                    matchIdList.add(matchId);
                 }
             }
 
+            for (String matchIdTmp: matchIdList) {
+                Element matchMessage = doc.getElementById(matchIdTmp);
+                matchMessageHeader = matchMessage.select("div[data-role=header] > h1");
+//                System.out.println(matchMessageHeader);
+                matchMessageContent = matchMessage.select("div[data-role=content] > a");
+
+                Map<String, String> messageTmp = new HashMap<String, String>();
+                Element tmp;
+                for (int i = 0; i < matchMessageHeader.size(); i++) {
+                    tmp = matchMessageHeader.get(i);
+                    if (i == 0) {
+                        messageTmp.put(MATCH_DATE,tmp.text());
+                    }
+                    if (i == 1) {
+                        messageTmp.put(MATCH_BATTLE,tmp.text());
+                    }
+                    if (i == 2) {
+                        tmp.select("span[style]").remove();
+                        messageTmp.put(MATCH_STATUS,tmp.text());
+                    }
+                }
+                dataList.add(messageTmp);
+
+                for (Element matchMessageContentTmp:matchMessageContent) {
+                    System.out.println(matchMessageContentTmp.attr("href"));
+                }
+            }
+            System.out.println(dataList.get(0));
         }
         catch (Exception e){
             e.printStackTrace();
